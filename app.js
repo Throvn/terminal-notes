e8396144f4c4b50ca1a7bfe6af64fbc5ce3aa3e5
throvn <Faceformer1>
2019-04-26 18:58:58 +0200

    added colors and removed an issue

--- app.js
+++ app.js
@@ -1,46 +1,54 @@
-const fs = require('fs')
-const _ = require('lodash')
-const yargs = require('yargs')
+const fs = require('fs');
+const chalk = require('chalk');
+const _ = require('lodash');
+const yargs = require('yargs');
 
-const notes = require('./notes.js')
-const yargsTitle = {title:{
-    describe: 'Title of note',
-    demand: true,
-    alias: 't'
-    }}
-const argv = yargs.command('add','add a new note', {
-    title: {
+const notes = require('./notes.js');
+const yargsTitle = {
+        title:{
         describe: 'Title of note',
         demand: true,
         alias: 't'
-    },
-    body: {
-        describe: 'Body of note',
-        demand: true,
-        alias: 'b'
+    }}
+const argv = yargs.command(
+    'add','add a new note', {
+        title: {
+            describe: 'Title of note',
+            demand: true,
+            alias: 't'
+        },
+        body: {
+            describe: 'Body of note',
+            demand: true,
+            alias: 'b'
+        }
     }
-})
+)
 .command(
-    'list', 'list all notes',
+    'list', 'list all notes'
+).command(
+    'read', 'Read the note',
     yargsTitle
-).command('read', 'Read the note', yargsTitle).command(
-    'remove', 'Remove Note by Title', yargsTitle
+).command(
+    'remove', 'Remove Note by Title',
+    yargsTitle
 ).help().argv
 const command = argv._[0]
 
 if (command === 'list') {
     var allNotes = notes.getAll();
-    console.log('Printing ',notes.getAll().length, ' note(s)')
+    console.log('Printing ',chalk.green.bold(notes.getAll().length), ' note(s)')
     allNotes.forEach((note) => {
-        console.log('----');
-        console.log('Title: '+ note.title);
-        console.log('Body: ' + note.body);
+        console.log(chalk.black.bold('------'));
+        console.log(chalk.bold('Title: ')+ note.title);
+        console.log(chalk.bold('Body: ') + note.body);
         
     });
 } else if (command === 'add') {
+    if(argv.title === true || argv.body === true) return console.log(chalk.bold.yellow('Note:')+' you have to write e.g. '+chalk.italic('-b=\"Example text\"'));
     var note = notes.addNote(argv.title, argv.body)
     if (note === undefined) {
-        console.log('Note Title already exists');
+        console.log(chalk.bold.yellow('Note:')+' Title already '+chalk.red('exists'));
         
     } else {
         console.log('Note created:'+note.title+', '+note.body);
@@ -48,19 +56,17 @@ if (command === 'list') {
     }
 } else if (command === 'remove') {
     var noteRemoved = notes.removeNote(argv.title)
-    var message = noteRemoved ? 'Note: '+ argv.title+ ' removed' : 'Note not found.'
+    var message = noteRemoved ? chalk.yellow.bold('Note: ')+chalk.bold( argv.title)+ chalk.red(' removed') : chalk.red('Note not found.')
     console.log(message);
-    
 } else if (command === 'read') {
     var note = notes.getNote(argv.title)
     
     if (note[0] === undefined) {
-        console.log('Note not found');
+        console.log(chalk.bold.red('Note not found'));
         
     } else {
-        
-        console.log('Note: ' + note[0].title + ', ' + note[0].body);
+        console.log(chalk.bold('Note: ') + note[0].title + ', ' + note[0].body);
     }
 } else {
-    console.log('command not found');
+    console.log(chalk.bold.yellow('NOTES:')+' command not found');
 }
